119cb4d1a4509d4cf9eb0d48f5237a1c6c8c1a2a
linhqtruong <linhbackup@gmail.com>
2022-03-03 02:08:02 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import MyComponent from "./MyComponent";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<MyComponent />, rootElement);
